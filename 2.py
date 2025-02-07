f75479a97ed2b452562a6f777457e82207e2881e
Smit-Sheliya <sheliyasmit113@gmail.com>
2025-02-07 11:26:41 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,4 @@
+a=int(input("enter a number of multiplicatuin table"))
+for i in range(1,11):
+    
+    print(f"{a}  *  {i}={a*i}")
---
+++ 3.py
@@ -0,0 +1,8 @@
+a=(input("enter a string:"))
+alphabets="abcdefghijklmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ"
+no_of_alphabets=0
+for char in a:
+    
+       no_of_alphabets+=1
+print("number of alphabets in a is ",no_of_alphabets)
+    
